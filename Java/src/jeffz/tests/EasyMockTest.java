f2cfb687b4905f187ccc19c63989bfcea684ec3e
Jeffrey Zhao <jeffz@live.com>
2010-04-06 23:40:17 +0800

    add EasyMock library

--- Java/src/jeffz/tests/EasyMockTest.java
+++ Java/src/jeffz/tests/EasyMockTest.java
@@ -1,10 +1,21 @@
 package jeffz.tests;
 
+import org.easymock.*;
+import static org.easymock.EasyMock.*;
+
 public class EasyMockTest {
 	public static void main(String[] args) {
-		Calculator mockObj = createMock(Calculator.class);
+		IMocksControl control = createControl();
+		
+		Calculator mockObj = control.createMock(Calculator.class);
 		expect(mockObj.add(1, 2)).andReturn(3);
 		expect(mockObj.add(4, 5)).andReturn(9);
 		expect(mockObj.divide(0, isA(Integer.class))).andReturn(0);
+		
+		control.replay();
+		
+		// pass to other methods
+		
+		control.verify();
 	}
 }
